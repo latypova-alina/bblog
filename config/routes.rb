0a213d9d66ed2fb63c2797974278ad141f783630
alina <alina.latypova@flatstack.com>
2019-02-19 16:36:53 +0300

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
 
   namespace :author do
-    resources :posts, only: %i[index new create edit update show]
+    resources :posts, only: %i[index new create edit update]
   end
 
   resources :posts, only: %i[index show]
