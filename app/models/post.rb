95be6c7e2bc1f0bd2992327b439a2233ea3fddea
latypova-alina <alinkin9696@mail.ru>
2019-03-23 00:42:13 +0300

    add sorting

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,9 @@ class Post < ApplicationRecord
   mount_uploader :image, PostImageUploader
 
   pg_search_scope :search, against: :title, using: { tsearch: { prefix: true } }
+
+  scope :with_likes, -> do
+    left_joins(:likes).group(:id)
+      .select("posts.*, count(likes.id) as likes_count")
+  end
 end
